2f158ab16f6ae1bb45715d239ba6f54064256470
KM Online Works <kmonlineworks@gmail.com>
2022-04-07 12:59:43 +0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import Card from './Card/Card';
-import { useEffect, useState } from 'react';
-import Pagination from './Pagination/Pagination';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+	CardCategory,
+	CardContainer,
+	CardDesc,
+	CardMedia,
+	CardMediaContainer,
+	CardTitle,
+} from '../src/Card/CardStyle';
 
 function App() {
-	const limit = 10;
 	const [products, setProducts] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	let lastPage = currentPage * limit;
-	let firstPage = lastPage - limit;
+	const [currentPage, setCurrentPage] = useState(10);
 
-	const paginate = (item) => {
-		setCurrentPage(item);
-	};
+	let observer = useRef();
+	const lastObservElement = useCallback(
+		(node) => {
+			if (observer.current) observer.current.disconnect();
+			observer.current = new IntersectionObserver(
+				(entries) => {
+					if (entries[0].isIntersecting) {
+						if (products.length === currentPage) return;
+						setCurrentPage((currentPage) => currentPage + 10);
+					}
+				},
+				{ root: null, rootMargin: '0px', threshold: 0.25 }
+			);
+			if (node) observer.current.observe(node);
+		},
+		[currentPage, products]
+	);
 
 	useEffect(() => {
 		const data = () => {
@@ -29,17 +46,34 @@ function App() {
 	return (
 		<div className="container">
 			<div className="grid-4">
-				{products?.slice(firstPage, lastPage).map((item, index) => (
-					<Card key={index + 1} product={item} />
-				))}
+				{products?.slice(0, currentPage).map((product, index) =>
+					currentPage === index + 1 ? (
+						<CardContainer key={product.id} ref={lastObservElement}>
+							<CardMediaContainer>
+								<CardMedia src={product.thumbnail} atl={product.title} />
+								<CardCategory>{product.category}</CardCategory>
+							</CardMediaContainer>
+							<CardTitle>{product.title}</CardTitle>
+							<CardDesc>{product.description}</CardDesc>
+						</CardContainer>
+					) : (
+						<CardContainer key={product.id}>
+							<CardMediaContainer>
+								<CardMedia src={product.thumbnail} atl={product.title} />
+								<CardCategory>{product.category}</CardCategory>
+							</CardMediaContainer>
+							<CardTitle>{product.title}</CardTitle>
+							<CardDesc>{product.description}</CardDesc>
+						</CardContainer>
+					)
+				)}
 			</div>
-			{products && (
-				<Pagination
-					products={products}
-					paginate={paginate}
-					currentPage={currentPage}
-					limit={limit}
-				/>
+			{products.length === currentPage && (
+				<h2
+					style={{ fontSize: '16px', textAlign: 'center', marginTop: '20px' }}
+				>
+					No more products..!
+				</h2>
 			)}
 		</div>
 	);
